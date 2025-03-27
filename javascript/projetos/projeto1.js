44d6865598bc484a157bf6b5fcb54a0a4ac8a3d0
matheus toti <matheustoti76@gmail.com>
2025-03-26 21:24:15 -0300

    oi

---
+++ javascript/projetos/projeto1.js
@@ -0,0 +1,22 @@
+var agora = new Date()
+
+var hora = agora.getHours()
+
+console.log(hora)
+
+var painel = document.getElementById("txthorario")
+
+painel.innerHTML = `agora são ${hora} horas.` 
+
+if (hora >= 6 && hora <= 12){
+    document.getElementById("variavelimg")
+    
+}
+
+
+
+
+
+
+
+
