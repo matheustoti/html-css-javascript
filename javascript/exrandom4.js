d794bc78f312b7c30cbf8582be996f9bdcdb7a57
matheus toti <matheustoti76@gmail.com>
2025-03-28 17:39:03 -0300

    oi

---
+++ javascript/exrandom4.js
@@ -0,0 +1,13 @@
+var vel = 60.5
+
+console.log("a velocidade do seu carro è "+(vel)+'km por hora')
+
+if (vel > 60){
+    console.log("voce ultrapassou a velocidade permitida. MULTADO!")
+}
+
+console.log("dirija com o cinto de segurança!")
+
+
+
+
---
+++ javascript/nasc.js
@@ -0,0 +1,4 @@
+function calculo(){
+    var atual = new Date().getFullYear()
+    var nasc = Number(atual) - Number(inasc.value)
+}
